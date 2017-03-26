c45ac05579e075631c245a4bab8e0b7fa0303143
Kees Kluskens <kees@webduck.nl>
2017-03-26 16:30:40 +0200

    Allow custom dotenv location

---
+++ packages/react-cy-scripts/scripts/utils/loadEnv.js
@@ -0,0 +1,7 @@
+// Load environment variables from .env file. Suppress warnings using silent
+// if this file is missing. dotenv will never modify any environment variables
+// that have already been set.
+// https://github.com/motdotla/dotenv
+require('dotenv').config({
+  path: process.env.CY_ENV_FILE || '.env'
+});
